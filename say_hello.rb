a074bbc148230465743383800e430889ddc79a2f
Antonio James <asjames18@gmail.com>
2020-07-06 18:42:48 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,4 @@
-# I call on the method, say_hello, and give it the string "Gabriela"
-say_hello("Gabriela")
+def say_hello("name")
+  puts ("Hello #{name}!")
 
-# The method prints the text tto the screen:
-say_hello 
+
